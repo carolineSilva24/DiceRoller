b45209aab22966e1c16a033f59c3503bd7fbdfec
carolinesilva03 <102956881+carolineSilva24@users.noreply.github.com>
2022-04-14 13:24:50 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function rolar(){
+  var inQnt = document.getElementById("number")
+  var numDados = inQnt.value
+  var inDice = document.querySelector('input[name="faces"]:checked')
+  var numFaces = inDice.value
+
+  var somatorio = 0
+  var i= 0
+  var rolagens = ""
+  while(i < numDados){
+    var rolagem = Math.floor(Math.random()*numFaces)+1
+    somatorio = somatorio + rolagem
+    rolagens = rolagens + rolagem + "-"
+    i=i+1
+  }
+  var  h3 = document.getElementById("soma")
+  h3.innerHTML = somatorio
+  var p = document.getElementById("rolagens")
+  p.innerHTML = rolagens
+}
